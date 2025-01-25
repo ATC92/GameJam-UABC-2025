68946d20f40784b85c18516c2eff96c7eb1aee6f
Alan Torres <158106033+ATC92@users.noreply.github.com>
2025-01-24 21:07:32 -0800

    Update seleccionador de Burbujas

--- GameJam/Assets/Scripts/circleSelector.cs
+++ GameJam/Assets/Scripts/circleSelector.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 
 public class circleSelector : MonoBehaviour
@@ -5,28 +6,29 @@ public class circleSelector : MonoBehaviour
     [Header("Lista de Objetos a girar")]
     [SerializeField] GameObject[] items;
     [SerializeField] float radius = 100f;
-    [SerializeField] float rotationSpeed = 100f;
+    [SerializeField] float rotationStep = 360f; // Grados de rotacion por paso
     private int selectedIndex = 0;
 
     void Start()
     {
-        ArrangeItemsInCircle();
-        HighlightSelectedItem();
+        ArrangeItemsInCircle(); // Distribuir los elementos al inicio
+        HighlightSelectedItem(); // Resaltar el primer elemento
     }
 
     void Update()
     {
-        // Cambiar selección con las teclas '1' y '3'
-        if (Input.GetKeyDown(KeyCode.Alpha1)) // Tecla '1'
+        // Cambiar rotacion con las teclas '1' y '3'
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // Tecla '1' para girar a la izquierda
         {
-            ChangeSelection(1); // Mover a la izquierda
+            RotateCircle(1); // Rotar en sentido horario
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha3)) // Tecla '3'
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) // Tecla '3' para girar a la derecha
         {
-            ChangeSelection(-1); // Mover a la derecha
+            RotateCircle(-1); // Rotar en sentido antihorario
         }
 
-        if (Input.GetKeyDown(KeyCode.Mouse0)) // Seleccion con "Click Izquierdo"
+        // Confirmar selección con "Enter"
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Debug.Log("Seleccionaste: " + items[selectedIndex].name);
         }
@@ -34,7 +36,7 @@ public class circleSelector : MonoBehaviour
 
     void ArrangeItemsInCircle()
     {
-        float angleStep = 360f / items.Length;
+        float angleStep = 360f / items.Length; // Angulo entre elementos
         for (int i = 0; i < items.Length; i++)
         {
             float angle = i * angleStep * Mathf.Deg2Rad;
@@ -44,29 +46,32 @@ public class circleSelector : MonoBehaviour
                 0
             );
             items[i].transform.localPosition = position;
+
+            // Ajustar la orientación del sprite para que mire hacia afuera
+            float angleDegrees = i * angleStep;
+            items[i].transform.localRotation = Quaternion.Euler(0, 0, angleDegrees - 45f);
+    
         }
     }
 
-    void ChangeSelection(int direction)
+    void RotateCircle(int direction)
     {
-        // Actualizar indice seleccionado
-        selectedIndex = (selectedIndex + direction + items.Length) % items.Length;
+        // Rotar el circulo completo
+        transform.Rotate(Vector3.forward, direction * (rotationStep / items.Length));
+
+        // Actualizar indice seleccionado (inverso porque rota el circulo, no el indice)
+        selectedIndex = (selectedIndex - direction + items.Length) % items.Length;
+
         HighlightSelectedItem();
     }
-
-    /*void RotateCircle(float angle)
-    {
-        transform.Rotate(Vector3.forward, angle);
-        selectedIndex = (selectedIndex - Mathf.RoundToInt(angle / (360f / items.Length)) + items.Length) % items.Length;
-        HighlightSelectedItem();
-    }*/
 
     void HighlightSelectedItem()
     {
         for (int i = 0; i < items.Length; i++)
         {
             SpriteRenderer renderer = items[i].GetComponent<SpriteRenderer>();
-            renderer.color = (i == selectedIndex) ? Color.yellow : Color.white; // Resalta el seleccionado
+            // Cambiar color: amarillo para seleccionado (parte superior), blanco para el resto
+            renderer.color = (i == selectedIndex) ? Color.yellow : Color.white;
         }
     }
 }
